0f820107422bb75bc150c812ca1bffce5fc743b7
Przemysław Rutkowski <rutkowski_przemyslaw@o2.pl>
2024-06-16 15:42:20 +0200

    Add more functionalities of cart

--- ShopApp/src/main/resources/static/js/cart.js
+++ ShopApp/src/main/resources/static/js/cart.js
@@ -8,10 +8,10 @@ if(!cart){
 addToCartButtons.forEach(button => {
     button.addEventListener("click", () => {
         const productId = parseInt(button.getAttribute("data-product-id"));
-        const productName = parseInt(button.getAttribute("data-product-name"));
-        const productDescription = parseInt(button.getAttribute("data-product-description"));
-        const productPrice = parseInt(button.getAttribute("data-product-price"));
-        addToCart(productId);
+        const productName = button.getAttribute("data-product-name");
+        const productDescription = button.getAttribute("data-product-description");
+        const productPrice = button.getAttribute("data-product-price");
+        addToCart(productId, productName, productDescription, productPrice);
         console.log(JSON.parse(localStorage.getItem("cart")));
     })
 })
@@ -24,18 +24,43 @@ function addToCart(productId, productName, productDescription, productPrice) {
     localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+function removeFromCart(productId) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    const product = cart.find(product => product.id === productId);
+    if(product) {
+        if(product.quantity === 1) cart = cart.filter(product => product.id !== productId);
+        else product.quantity -= 1;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    displayCart();
+}
+
 function displayCart(){
     const cart = JSON.parse(localStorage.getItem("cart"));
     const cartList = document.querySelector(".cart-list");
+    cartList.innerHTML = "";
     cart.forEach(product => {
         const productElement = document.createElement("div");
         productElement.classList.add("product");
-        productElement.innerHTML = `
-            <h3>${product.name}<h3>
-            <p>Description: ${product.description}</p>
-            <p>Price: ${product.price}</p>
-            <p>Quantity: ${product.quantity}</p>
-        `;
+        productElement.setAttribute("data-product-id", product.id.toString());
+
+        const productNameElement = document.createElement("p");
+        productNameElement.innerText = product.name;
+
+        const productDescriptionElement = document.createElement("p");
+        productDescriptionElement.innerText = product.description;
+
+        const productPriceElement = document.createElement("p");
+        productPriceElement.innerText = product.price;
+
+        const productQuantityElement = document.createElement("p");
+        productQuantityElement.innerText = product.quantity;
+
+        productElement.appendChild(productNameElement);
+        productElement.appendChild(productDescriptionElement);
+        productElement.appendChild(productPriceElement);
+        productElement.appendChild(productQuantityElement);
+
         cartList.appendChild(productElement);
     })
 }
